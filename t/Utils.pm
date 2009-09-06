df93ef724f52e03bf8e7074c6dabafc695bb71be
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-09-07 01:08:17 +0900

    test should pass $env cleanly

--- t/Utils.pm
+++ t/Utils.pm
@@ -15,6 +15,7 @@ sub req {
         'psgi.errors'     => *STDERR,
         'psgi.url_scheme' => ($ENV{HTTPS}||'off') =~ /^(?:on|1)$/i ? 'https' : 'http',
         %ENV,
+        %{ delete($args{env}) || +{} },
     };
     PlackX::Request->new($env, %args);
 }
