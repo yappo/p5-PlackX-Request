d3b69264f4eac0682b5ec20fc8673483ca30e190
Tokuhiro Matsuno <tokuhirom@gmail.com>
2009-09-07 01:44:29 +0900

    default psgi.input for test as PerlIO

--- t/Utils.pm
+++ t/Utils.pm
@@ -9,9 +9,10 @@ our @EXPORT = qw/ req /;
 sub req {
     my %args = @_;
 
+    open my $in, '<', \do { my $d };
     my $env = {
         'psgi.version'    => [ 1, 0 ],
-        'psgi.input'      => *STDIN,
+        'psgi.input'      => $in,
         'psgi.errors'     => *STDERR,
         'psgi.url_scheme' => ($ENV{HTTPS}||'off') =~ /^(?:on|1)$/i ? 'https' : 'http',
         %ENV,
